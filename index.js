7ecca3f46efc9b11b6d184439fbf9324565800a6
JosephMainaOtundo <otundomainajoseph@gmail.com>
2024-04-17 13:14:40 +0300

    complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    return document.getElementById('text').textContent = "This is really cool!";
+    
+  });
+   
